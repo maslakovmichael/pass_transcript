6eb5a5e8469ecdbfa491e9e2c18c7fe1168cf9f7
Worker <worker@gmail.com>
2021-03-02 18:42:22 +0200

    Create function for class Translater

--- transcript.py
+++ transcript.py
@@ -23,10 +23,56 @@ letters_dict_2 = {'а': 'f', 'б': ',', 'в': 'd', 'г': 'u', 'д': 'l', 'е': '
 C = '#c7bcf3'
 
 class Translater:
-    def __init__(self):
-        pass
+    def __init__(self, root):
+        self.entry_int = tk.Entry(root)
 
 
+        self.lab_out1 = tk.Label(root)
+        self.lab_out2 = tk.Label(root)
+
+    def clearing(self):
+        self.entry_int.delete(0, 'end')
+        self.lab_out1.configure(text='поле для результата')
+        self.lab_out2.configure(text='поле для результата')
+
+    def past_text(self):
+        pas = tk.Tk()
+        pas.withdraw()
+        clip = pas.clipboard_get()
+        self.entry_int.delete(0, tk.END)
+        self.entry_int.insert(0, clip)
+        pas.update()
+        pas.destroy()
+
+    def copy_text1(self):
+        cop = tk.Tk()
+        cop.withdraw()
+        cop.clipboard_clear()
+        cop.clipboard_append(self.lab_out1['text'])
+        cop.update()
+        cop.destroy()
+
+    def copy_test2(self):
+        cop = tk.Tk()
+        cop.withdraw()
+        cop.clipboard_clear()
+        cop.clipboard_append(self.lab_out2['text'])
+        cop.update()
+        cop.destroy()
+
+    def translate(self):
+        st = self.entry_int.get()
+        s1 = ''
+        s2 = ''
+        for i in st:
+            if i in letters_dict_1:
+                s1 += letters_dict_1[i]
+                s2 += letters_dict_2[i]
+            else:
+                s1 += i
+                s2 += i
+        self.lab_out1['text'] = s1
+        self.lab_out2['text'] = s2
 
 
 

35d31f93b98a2aac77f664a15c83003b76cdb20b
Worker <worker@gmail.com>
2021-03-02 19:37:35 +0200

    Add comments

--- transcript.py
+++ transcript.py
@@ -1,3 +1,5 @@
+#!/bin/python3
+
 import tkinter as tk
 
 letters_dict_1 = {'а': 'f', 'б': ',', 'в': 'd', 'г': 'u', 'д': 'l', 'е': 't', 'ё': '`', 'ж': ';',
@@ -23,6 +25,7 @@ letters_dict_2 = {'а': 'f', 'б': ',', 'в': 'd', 'г': 'u', 'д': 'l', 'е': '
 C = '#c7bcf3'
 B = 2
 
+'''Класс Translater - определяет компоненты визуального интерфейса, их свойства и параметры'''
 class Translater:
     def __init__(self, root):
         self.lab_text_int = tk.Label(root, text='Password: ')
@@ -31,7 +34,6 @@ class Translater:
         self.lab_text_int.grid(row=0, column=0, sticky='we')
         self.entry_int.grid(row=0, column=1, sticky='we')
         self.btn_paste_in.grid(row=0, column=2, sticky='we')
-
 
         self.btn_apply = tk.Button(root, text='Apply', bd=B, bg=C, command=self.translate)
         self.btn_apply.grid(row=1, column=1, sticky='we')
@@ -45,7 +47,7 @@ class Translater:
 
         self.lab_text_out2 = tk.Label(root, text='Transcription 2: ')
         self.lab_out2 = tk.Label(root, text='поле для результата', width=55, font=15)
-        self.btn_copy_out2 = tk.Button(root, text='Copy 2', bd=B, bg=C, command=self.copy_test2)
+        self.btn_copy_out2 = tk.Button(root, text='Copy 2', bd=B, bg=C, command=self.copy_text2)
         self.lab_text_out2.grid(row=3, column=0)
         self.lab_out2.grid(row=3, column=1)
         self.btn_copy_out2.grid(row=3, column=2)
@@ -53,20 +55,23 @@ class Translater:
         self.btn_clear = tk.Button(root, text='Clear', bd=B, bg=C, command=self.clearing)
         self.btn_clear.grid(row=4, column=1, sticky='we')
 
+# Метод clearing() для очистки полей от текста
     def clearing(self):
         self.entry_int.delete(0, 'end')
         self.lab_out1.configure(text='поле для результата')
         self.lab_out2.configure(text='поле для результата')
 
+# Метод past_text() для вставки текста в поле self.entry_int из буфера обмена
     def past_text(self):
         pas = tk.Tk()
         pas.withdraw()
-        clip = pas.clipboard_get()
+        clip = pas.clipboard_get()       # получаем содержимое буфера обмена в обьект clip
         self.entry_int.delete(0, tk.END)
-        self.entry_int.insert(0, clip)
+        self.entry_int.insert(0, clip)   # помещаем содержимое в окно
         pas.update()
         pas.destroy()
 
+# Метод copy_text1() для копирования текста из поля self.lab_out1 в буфер обмена
     def copy_text1(self):
         cop = tk.Tk()
         cop.withdraw()
@@ -75,7 +80,8 @@ class Translater:
         cop.update()
         cop.destroy()
 
-    def copy_test2(self):
+# Метод copy_text2() для копирования текста из поля self.lab_out2 в буфер обмена
+    def copy_text2(self):
         cop = tk.Tk()
         cop.withdraw()
         cop.clipboard_clear()
@@ -83,6 +89,7 @@ class Translater:
         cop.update()
         cop.destroy()
 
+# Метод translate() для совершения транскрипирования текста по нажатию кнопки Apply
     def translate(self):
         st = self.entry_int.get()
         s1 = ''
@@ -96,8 +103,6 @@ class Translater:
                 s2 += i
         self.lab_out1['text'] = s1
         self.lab_out2['text'] = s2
-
-
 
 if __name__=='__main__':
     root = tk.Tk()
